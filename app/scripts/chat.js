d7a7044f5698a372af36a4e4686042c925d980f1
yongseongjeon <yongseong.jeon95@gmail.com>
2019-11-05 14:05:44 +0900

    채팅방의 채팅 인터랙션, 봇 메뉴 인터랙션 추가합니다, 봇 메뉴 호출 아이콘 수정해야 합니다.

--- app/scripts/chat.js
+++ app/scripts/chat.js
@@ -1,40 +1,90 @@
 // 햄버거 바 인터랙션
 const handleHamburgerBar = () => {
-    $(".hamburger-open-btn").click(() => {
-        $(".hamburger-bar").show();
-    })
-    $(".close-bamburger").click(() => {
-        $(".hamburger-bar").hide();
-    })
+  $(".hamburger-open-btn").click(() => {
+    $(".hamburger-bar").show();
+  })
+  $(".close-bamburger").click(() => {
+    $(".hamburger-bar").hide();
+  })
 }
 
 // 봇 메뉴 인터랙션
 const handleBotMenu = () => {
+  const showBotMenu = () => {
+    $(".bot-menus").show();
+    $(".call-bot").hide();
+    $(".close-bot").show();
+  }
+  const hideBotMenu = () => {
     $(".bot-menus").hide();
+    $(".call-bot").show();
     $(".close-bot").hide();
-    $(".call-bot").click(() => {
-        $(".bot-menus").show();
-        $(".call-bot").hide();
-        $(".close-bot").show();
+  }
+  $(".bot-menus").hide();
+  $(".close-bot").hide();
+  $(".call-bot").click(() => {
+    showBotMenu();
+  })
+  $(".close-bot").click(() => {
+    hideBotMenu();
+  })
+
+  $(".bot-menu").click(() => {
+    const selectedId = event.currentTarget.id;
+    $(`#${selectedId}`).css({
+      "color": "#d86b1f",
+      "border": "solid 1px #d86b1f"
     })
-    $(".close-bot").click(() => {
-        $(".bot-menus").hide();
-        $(".call-bot").show();
-        $(".close-bot").hide();
-    })
+    // 토픽 선택 0.5초 후 색 초기화 및 봇 메뉴 사라짐
+    setTimeout(() => {
+      hideBotMenu();
+      $(`#${selectedId}`).css({
+        "color": "#666666",
+        "border": "solid 1px white"
+      })
+    }, 500);
+  })
 }
 
 // 채팅 입력 인터랙션
 const handleInputChatting = () => {
-    $(".text-input").keyup(() => {
-        const isEmptyInput = !$(".text-input").val().length;
-        if (isEmptyInput) {
-            $(".send").css("background-color", "#c8c8c8");
-        }
-        else {
-            $(".send").css("background-color", "#d86b1f");
-        }
-    })
+  $(".text-input").keyup(() => {
+    const isEmptyInput = !$(".text-input").val().length;
+    if (isEmptyInput) {
+      $(".send").css("background-color", "#c8c8c8");
+    }
+    else {
+      $(".send").css("background-color", "#d86b1f");
+    }
+  })
+}
+
+// 채팅 입력
+const addChatting = () => {
+  const chatContents = $(".row");
+  $(".send").click(() => {
+    const inputText = $(".text-input").val();
+    const chatForm = `<div class="mine-chat-row">
+                        <div class="time-and-balloon">
+                          <div class="time">23:02</div>
+                          <div class="mine-speech-balloon">
+                            <div class="text">${inputText}</div>
+                          </div>
+                        </div>
+                      </div>`;
+    // 최하단에 위치한 채팅이 내 채팅일 때 && 채팅내용이 있을 때
+    if ($(".row").children().last()[0].className !== "mine-chat" && inputText.length) {
+      chatContents.append(`<div class="mine-chat">
+                            ${chatForm}
+                          </div>`);
+    }
+    else if (inputText.length) {
+      $(".row").children().last().append(chatForm);
+    }
+    // 입력창 초기화
+    $(".text-input").val("");
+    $(".send").css("background-color", "#c8c8c8");
+  })
 }
 
 // DOM 렌더링 완료된 후 실행
@@ -42,4 +92,5 @@ $(() => {
     handleHamburgerBar();
     handleBotMenu();
     handleInputChatting();
+    addChatting();
 });
